b1012b2c8a33143832d19a2f8210e93bfe642ac2
yasi <zahrafarrokhi2017@gmail.com>
2022-05-19 19:11:06 +0430

    add back icon and style for layout

--- frontend/frontend/components/LoginLayout.jsx
+++ frontend/frontend/components/LoginLayout.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import styles from '../styles/loginLayout.module.scss';
+import { IoChevronBackCircle } from 'react-icons/io5';
+// import { IoArrowBackCircleOutline } from 'react-icons/io5';
+import {useRouter} from 'next/dist/client/router';
 
 const LoginLayout = (props) => {
- const { children} = props;
-
+ const { children ,backlink} = props;
+const router = useRouter();
   return(
    <div className={`d-flex flex-column align-items-center justify-content-center w-100 ${styles.maincnt}`}>
     <div className={`d-flex flex-colum w-100 ${styles.cnt}`}>
+    <IoChevronBackCircle
+          className={backlink ? styles.back_link : "d-none"}
+          onClick={() => router.back()}
+        />
        {children}
     </div>
    </div>
