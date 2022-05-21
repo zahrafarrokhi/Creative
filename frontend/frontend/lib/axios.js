4c004c6bcdfdfdf634506caaff5d71e0111527b1
zahrafarrokhi <zahrafarrokhi2017@gmail.com>
2022-05-21 14:20:29 +0430

    install axios and axios-auth-refresh

---
+++ frontend/frontend/lib/axios.js
@@ -0,0 +1,48 @@
+import axios from 'axios';
+import createAuthRefreshInterceptor from 'axios-auth-refresh';
+import * as cookie from 'cookie';
+import * as setCookie from 'set-cookie-parser';
+
+// Create axios instance.
+const axiosInstance = axios.create({
+  // baseURL: process.env.BACKEND_BASE_URL,  #becuse for confing on next.config.js and traefik
+  withCredentials: true,
+  headers: {
+    common: {
+      'Accept-Language': 'ir',
+    },
+  },
+});
+
+export const setupInterceptors = (store) => {
+
+
+  createAuthRefreshInterceptor(axiosInstance, (failedRequest) => {
+    return axiosInstance
+    // TokenRefreshSerializer ,220,221
+      .post('/api/auth/refresh/', {
+        user_id: store.getState().authReducer?.username,
+        refresh: store.getState().authReducer?.refreshToken,
+      })
+      .then((resp) => {
+        // TokenRefreshSerializer ,237
+        const { access_tok: accessToken } = resp.data;
+        const bearer = `${
+          process.env.JWT_AUTH_HEADER ?? 'Bearer'
+        } ${accessToken}`;
+        console.log(accessToken);
+        axiosInstance.defaults.headers.Authorization = bearer;
+
+       
+        failedRequest.response.config.headers.Authorization = bearer;
+        return Promise.resolve();
+      })
+    }
+  );
+};
+
+
+
+
+// Create axios interceptor
+export default axiosInstance;
