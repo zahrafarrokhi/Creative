52bc0c386ad869ff9c077c4f8e83d395121be4df
zahrafarrokhi <zahrafarrokhi2017@gmail.com>
2022-05-21 16:27:11 +0430

    remove cookie

--- frontend/frontend/lib/axios.js
+++ frontend/frontend/lib/axios.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
 import createAuthRefreshInterceptor from 'axios-auth-refresh';
-import * as cookie from 'cookie';
-import * as setCookie from 'set-cookie-parser';
 
 // Create axios instance.
 const axiosInstance = axios.create({
