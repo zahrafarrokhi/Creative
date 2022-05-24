77a84917693d1b915dbdbbbbe1e78eedf0e35342
zahrafarrokhi <zahrafarrokhi2017@gmail.com>
2022-05-24 15:19:49 +0430

    add splash

--- frontend/frontend/components/SplashScreen.jsx
+++ frontend/frontend/components/SplashScreen.jsx
@@ -3,9 +3,20 @@ import { animate, motion, useMotionValue } from "framer-motion";
 import Image from "next/image";
 import { IoCalendarOutline } from "react-icons/io5";
 import { useRouter } from "next/dist/client/router";
-// import styles from "../styles/SplashScreen.module.scss";
+import styles from "../styles/SplashScreen.module.scss";
 import Link from "next/link";
 
+const WALKTHROUGH_ICONS = [
+  "/walkthrough01.png",
+  "/walkthrough02.png",
+  "/walkthrough03.png",
+];
+
+const WALKTHROUGH_TEXTS = [
+  "ویزیت با بهترین پزشکان فوق تخصص",
+  "سفارش آنلاین دارو و ارسال رایگان",
+  "نمونه‌گیری آزمایش در منزل",
+];
 const pageStyle = {
   position: "absolute",
   width: "100%",
@@ -27,7 +38,6 @@ export const Page = (props) => {
       style={{
         ...pageStyle,
         x,
-        y,
         left: `${index * 100}%`,
       }}
       className="w-100 d-flex align-items-center justify-content-center"
@@ -38,6 +48,31 @@ export const Page = (props) => {
     >
       {child}
     </motion.div>
+  );
+};
+const Dots = (props) => {
+  const { index, setIndex, count } = props;
+
+  const dots = useMemo(() => [...Array(count).keys()], [count]);
+
+  console.log("Dot count", count, dots);
+
+  return (
+    <div
+      className={`d-flex flex-row-reverse justify-content-center ${styles.dotscnt}`}
+    >
+      {dots.map((_, ind) => {
+        console.log("Rendering dots", ind, count);
+        return (
+          <div
+            className={`${styles.dot} ${
+              (count + (index % count)) % count === ind ? styles.dotactive : ""
+            }`}
+            onClick={() => setIndex(ind)}
+          ></div>
+        );
+      })}
+    </div>
   );
 };
 
@@ -76,11 +111,11 @@ const VirtualizedPage = ({ children, count, index, setIndex }) => {
     }
 
     if (offset.x > clientWidth / 4) {
-      setIndex(index - 1);
-      // else animate(x, calculateNewX(), transition);
+      if (index > 0) setIndex(index - 1);
+      else animate(x, calculateNewX(), transition);
     } else if (offset.x < -clientWidth / 4) {
-      setIndex(index + 1);
-      // else animate(x, calculateNewX(), transition);
+      if (index < count - 1) setIndex(index + 1);
+      else animate(x, calculateNewX(), transition);
     } else {
       animate(x, calculateNewX(), transition);
     }
@@ -115,4 +150,77 @@ const VirtualizedPage = ({ children, count, index, setIndex }) => {
   );
 };
 
-export default VirtualizedPage;
+const NotificationCarousel = (props) => {
+  const [index, setIndex] = useState(0);
+  const [isOpen, setOpen] = useState(
+    localStorage.getItem("seen_splash") !== "1"
+  );
+
+  return (
+    <div
+      className={`position-relative d-flex d-md-none flex-column justify-content-start align-items-stretch ${
+        styles.cnt
+      } ${isOpen ? "" : "d-none"}`}
+    >
+      <div
+        className={`d-flex flex-row justify-content-center align-items-center ${styles.header}`}
+      >
+        {/* <MainIcon alt="کینیک غدد" className={`${styles.mainicn}`} /> */}
+      </div>
+      <div
+        className={`d-flex justify-content-center align-items-center ${styles.carousel}`}
+      >
+        <VirtualizedPage
+          count={WALKTHROUGH_ICONS.length}
+          index={index}
+          setIndex={setIndex}
+        >
+          {({ index }) => {
+            if (index >= WALKTHROUGH_ICONS.length || index < 0) {
+              return <div></div>;
+            }
+            const Icon = { icon: WALKTHROUGH_ICONS[index] };
+            console.log(index, Icon);
+            const Text = WALKTHROUGH_TEXTS[index];
+            return (
+              <div
+                className={`d-flex flex-column align-items-center ${styles.pagecnt}`}
+              >
+                {/*<Icon.icon className={`${styles.icon}`}/>*/}
+                <Image src={Icon.icon} width={"400px"} height={"400px"} />
+                <div className={`${styles.text}`}>{Text}</div>
+              </div>
+            );
+          }}
+        </VirtualizedPage>
+      </div>
+      <div className={`d-flex flex-column m-2 ${styles.btncnt}`}>
+        <div
+          className={`align-self-end ${index < 2 ? "" : "d-none"} ${
+            styles.link
+          }`}
+          onClick={() => {
+            setOpen(false);
+            localStorage.setItem("seen_splash", 1);
+            // setIndex(index + 1)
+          }}
+        >
+          ردکردن
+        </div>
+        <div
+          className={`align-self-start ${styles.link} ${
+            index == 2 ? "" : "d-none"
+          }`}
+          onClick={() => {
+            setOpen(false);
+            localStorage.setItem("seen_splash", 1);
+          }}
+        >
+          ثبت‌نام
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NotificationCarousel;
--- frontend/frontend/pages/index.js
+++ frontend/frontend/pages/index.js
@@ -1,20 +1,26 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import VirtualizedPage from '../components/SplashScreen'
+// import VirtualizedPage from '../components/SplashScreen'
 import { useState } from 'react'
+import dynamic from 'next/dynamic';
+
 
 export default function Home() {
   const [index, setIndex] = useState(0)
+<<<<<<< Updated upstream
   // const test = (a) => { // a = {index: 1, value: ''}
   //   const {index, value} = a;
   // }
   // const index = 4, value = 'aa'
   // test({index, value})
   // const test = ({index, value})
+=======
+  const VirtualizedPage = dynamic(() => import('../components/SplashScreen'), {ssr: false})
+>>>>>>> Stashed changes
   return (
     <div className='w-100 h-100 d-flex justify-content-center align-items-center'>
-    <div style={{width: '100px', height: '100px'}}>
+    <div >
      <VirtualizedPage index={index} setIndex={setIndex}>
        {( indexmy ) => {
          return <div style={{width: '100%', height: '100%', backgroundColor: indexmy % 2 ? 'red' : 'blue'}}>
