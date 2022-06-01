081a09647bea9a36a151073bfbff55a1fa309c96
zahrafarrokhi <zahrafarrokhi2017@gmail.com>
2022-06-01 13:02:26 +0430

    check permission for list

--- backend/constant_data/views.py
+++ backend/constant_data/views.py
@@ -2,8 +2,15 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import City, SupplementaryInsurance
 from .serializers import CitySerializer, SupplementaryInsuranceSerializer
-
-
+"""
+class ReadOnlyModelViewSet(mixins.RetrieveModelMixin,
+                           mixins.ListModelMixin,
+                           GenericViewSet):
+  
+    pass
+ ReadOnlyModelViewSet => has list but  queryset = City.objects.all() doesn't mater check permission for list, 
+            meaning that anyone can/should view these items, so we don't need any specific permission defined 
+"""
 class CityViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = City.objects.all()
     serializer_class = CitySerializer
