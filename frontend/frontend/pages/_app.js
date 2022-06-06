7a941b029d3e0c815127669239f27d9ad504e720
zahrafarrokhi <zahrafarrokhi2017@gmail.com>
2022-06-06 15:46:12 +0430

    Add axios interceptors Add persist gate

--- frontend/frontend/pages/_app.js
+++ frontend/frontend/pages/_app.js
@@ -1,10 +1,18 @@
+import { useStore } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { setupInterceptors } from '../lib/axios'
 import { wrapper } from '../lib/store'
 import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page)
+  const store = useStore();
+  setupInterceptors(store)
 
-  return getLayout(<Component {...pageProps} />)
+  return <PersistGate persistor={store.__PERSISTOR} loading={null}>
+
+   {getLayout(<Component {...pageProps} />)}
+   </PersistGate>
 }
 
 export default wrapper.withRedux(MyApp)
